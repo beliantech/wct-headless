6f22360dae4f82acc7271ed2d028259091e04417
ishmael.ahmed <ishmael.ahmed@readingplus.com>
2017-02-20 11:37:27 -0500

    having the container cleanup after itself

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 export DISPLAY=:99
 Xvfb :99 -shmem -screen 0 1024x768x24 &
-su - seleuser -c "cd /opt/code/; bower install; wct test"
+su - seleuser -c "cd /opt/code/; bower install; wct test; rm -rf bower_components;"
 
