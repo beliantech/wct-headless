9d4550be6a01b58888acbeeef23f601d39598da4
ishmael.ahmed <ishmael.ahmed@readingplus.com>
2017-02-20 09:19:48 -0500

    adding --allow-root to the bower command to allow bower to be run as root user

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,6 @@
 export DISPLAY=:99
 Xvfb :99 -shmem -screen 0 1024x768x24 &
-# x11vnc -passwd secret -display :99 -N -forever &
-# selenium must be started by a non-root user otherwise chrome can't start
-# su - seleuser -c "selenium-standalone start" &
 cd /opt/code/;
-bower install;
+bower install --allow-root;
 wct test;
-# su - seleuser -c "cd /opt/code/; bower install; wct test"
 
