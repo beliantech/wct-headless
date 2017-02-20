dfee8c600edad96cea8e491cbbba8c4cf0004979
ishmael.ahmed <ishmael.ahmed@readingplus.com>
2017-02-20 09:01:50 -0500

    now running as root user

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,8 @@ Xvfb :99 -shmem -screen 0 1024x768x24 &
 # x11vnc -passwd secret -display :99 -N -forever &
 # selenium must be started by a non-root user otherwise chrome can't start
 # su - seleuser -c "selenium-standalone start" &
-su - seleuser -c "cd /opt/code/; bower install; wct test"
+cd /opt/code/;
+bower install;
+wct test;
+# su - seleuser -c "cd /opt/code/; bower install; wct test"
 
