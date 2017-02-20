f4eb4cace435d7b7e84d09579c1e184c214c212e
ishmael.ahmed <ishmael.ahmed@readingplus.com>
2017-02-20 09:49:08 -0500

    reverting back to what works locally, moving the permission setting to the ci

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,4 @@
 export DISPLAY=:99
 Xvfb :99 -shmem -screen 0 1024x768x24 &
-cd /opt/code/;
-bower install --allow-root;
-su - seleuser -c "wct test"
+su - seleuser -c "cd /opt/code/; bower install; wct test"
 
